feaf1638e66a5144a1e5059dcfab6a6a38b9674a
kbolelyy <kbolelyy@gmail.com>
2017-01-24 21:58:34 +0700

    test1

--- app-gen/src/code/Hello.java
+++ app-gen/src/code/Hello.java
@@ -3,5 +3,10 @@ package code;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hurrrey!");
+        System.out.println("Hurrrey!");
+        System.out.println("Hurrrey!");
+        System.out.println("Hurrrey!");
+        System.out.println("Hurrrey!");
+        System.out.println("Hurrrey!");
     }
 }
