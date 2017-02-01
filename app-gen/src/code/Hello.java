8a1b4d7fd576ed0174c9d0dcebdc5cadea771618
Kirill Bolely <kbolelyy@gmail.com>
2017-02-01 22:57:04 +0700

    test1

--- app-gen/src/code/Hello.java
+++ app-gen/src/code/Hello.java
@@ -1,14 +1,5 @@
 package code;
 
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hurrrey!Hurrrey!Hurrrey!Hurrrey!Hurrrey!");
-        System.out.println("Hurrrey!Hurrrey!Hurrrey!Hurrrey!Hurrrey!");
-        System.out.println("Hurrrey!Hurrrey!Hurrrey!Hurrrey!Hurrrey!");
-        System.out.println("Hurrrey!Hurrrey!Hurrrey!Hurrrey!Hurrrey!");
-        System.out.println("Hurrrey!Hurrrey!Hurrrey!Hurrrey!Hurrrey!");
-        System.out.println("Hurrrey!Hurrrey!Hurrrey!Hurrrey!Hurrrey!");
-        System.out.println("Hurrrey!Hurrrey!Hurrrey!Hurrrey!Hurrrey!");
-        System.out.println("Hurrrey!Hurrrey!Hurrrey!Hurrrey!Hurrrey!");
-    }
+
 }
