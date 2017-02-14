fa846132307c6531087c5e95f83db705a5c5a43b
kbolelyy <kbolelyy@gmail.com>
2017-02-14 20:45:18 +0700

    test3

--- app-gen/src/code/Hello.java
+++ app-gen/src/code/Hello.java
@@ -3,12 +3,5 @@ package code;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hurrrey!Hurrrey!Hurrrey!Hurrrey!Hurrrey!");
-        System.out.println("Hurrrey!Hurrrey!Hurrrey!Hurrrey!Hurrrey!");
-        System.out.println("Hurrrey!Hurrrey!Hurrrey!Hurrrey!Hurrrey!");
-        System.out.println("Hurrrey!Hurrrey!Hurrrey!Hurrrey!Hurrrey!");
-        System.out.println("Hurrrey!Hurrrey!Hurrrey!Hurrrey!Hurrrey!");
-        System.out.println("Hurrrey!Hurrrey!Hurrrey!Hurrrey!Hurrrey!");
-        System.out.println("Hurrrey!Hurrrey!Hurrrey!Hurrrey!Hurrrey!");
-        System.out.println("Hurrrey!Hurrrey!Hurrrey!Hurrrey!Hurrrey!");
     }
 }
